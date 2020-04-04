1c98fcb1b5e7733642fdce5911b70927cbd0e435
JordanSaurav <janardhansingh1998@gmail.com>
2020-04-04 11:00:19 +0530

    "signup page created and data has been added to database"

--- index.php
+++ index.php
@@ -3,16 +3,72 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+    <title>INT301 - PHP PROJECT</title>
 </head>
 <body>
-    <h1>First program of PHP</h1>
+<div class="signup-form">
+    <form method="post">
+		<h2>Sign Up</h2>
+		<p>Please fill in this form to create an account!</p>
+		<hr>
+        <div class="form-group">
+			<div class="input-group">
+				<span class="input-group-addon"><i class="fa fa-user"></i></span>
+				<input type="text" class="form-control" name="username" placeholder="Username" required="required">
+			</div>
+        </div>
+        <div class="form-group">
+			<div class="input-group">
+				<span class="input-group-addon"><i class="fa fa-paper-plane"></i></span>
+				<input type="email" class="form-control" name="email" placeholder="Email Address" required="required">
+			</div>
+        </div>
+		<div class="form-group">
+			<div class="input-group">
+				<span class="input-group-addon"><i class="fa fa-lock"></i></span>
+				<input type="password" class="form-control" name="password" placeholder="Password" required="required">
+			</div>
+        </div>
+		<!--<div class="form-group">
+			<div class="input-group">
+				<span class="input-group-addon">
+					<i class="fa fa-lock"></i>
+					<i class="fa fa-check"></i>
+				</span>
+				<input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password" required="required">
+			</div>
+        </div>-->
+        <div class="form-group">
+			<label class="checkbox-inline"><input type="checkbox" required="required"> I accept the <a href="#">Terms of Use</a> &amp; <a href="#">Privacy Policy</a></label>
+		</div>
+		<div class="form-group">
+            <button type="submit" name="submit" class="btn btn-primary btn-lg">Sign Up</button>
+        </div>
+    </form>
 
-    <?php
-    
-    
-    ?>
+	<?php	
+		if (isset($_POST['submit'])) {
+			$conn = mysqli_connect('localhost', 'root', '', 'jordan') or die("Connection failed: " . mysqli_connect_error());
+			$username = $_POST['username'];
+			$email = $_POST['email'];
+			$pass = $_POST['password'];
 
-    
+			$sql = "insert into int301(username, email, password) values('$username', '$email', '$pass')";
+			if (mysqli_query($conn, $sql)) {
+				echo "Signup Successful!";
+				echo "<br>";
+				echo "New record created successfully.";
+			} else {
+				echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+			}
+		}
+	 ?>
+</div>
 </body>
 </html>
